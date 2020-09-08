748be0755aef21927e81bb2498ad7c82f63eb9d4
romulo <romulogarofalo@gmail.com>
2020-09-08 16:57:42 -0300

    finished styles of cube

--- cube.js
+++ cube.js
@@ -5,6 +5,10 @@ canvas.height = window.innerHeight;
 document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 
+const clt = [1,2,3]; const crmt = [1,2]; 
+ 
+
+
 // Pick out the form elements for easy access later
 var x1 = 100;
 var x2 = 100;
@@ -293,13 +297,87 @@ function drawCube(x, y, wx, wy, h, color) {
   //top left middle
   ctx.beginPath();
   ctx.moveTo(x -300, y - h -150);
-  ctx.lineTo(x - wx, y - h - wx * 0.5);
-  ctx.lineTo(x - wx + wy, y - h - (wx * 0.5 + wy * 0.5));
+  ctx.lineTo(x - wx -100, y - h - wx * 0.5 -50);
+  ctx.lineTo(x - wx + wy -100, y - h - (wx * 0.5 + wy * 0.5) -50);
   ctx.lineTo(x + wy -300, y - h - wy * 0.5 -150);
   ctx.closePath();
-  ctx.fillStyle = shadeColor('#dddddd', -20);
-  ctx.strokeStyle = shadeColor('#dddddd', -60);
+  ctx.fillStyle = shadeColor('#dddf99', -20);
+  ctx.strokeStyle = shadeColor('#dddf99', -60);
   ctx.stroke();
   ctx.fill();
+
+  //top middle middle (fix on back)
+  ctx.beginPath();
+  ctx.moveTo(x -100, y - h-250);
+  ctx.lineTo(x + wy -300, y - h - wy * 0.5 -150);
+  ctx.lineTo(x - wx , y - h - wx * 0.5 -100);
+  ctx.lineTo(x - wx + wy , y - h - (wx * 0.5 + wy * 0.5) -100);
+  ctx.closePath();
+  ctx.fillStyle = shadeColor('#333f99', -20);
+  ctx.strokeStyle = shadeColor('#333f99', -60);
+  ctx.stroke();
+  ctx.fill();
+
+  //top middle middle (fix on back)
+  ctx.beginPath();
+  ctx.moveTo(x + 100, y - h-250);
+  ctx.lineTo(x + wy -100, y - h - wy * 0.5 -250);
+  ctx.lineTo(x - wx , y - h - wx * 0.5 -200);
+  ctx.lineTo(x - wx + wy , y - h - (wx * 0.5 + wy * 0.5) -100);
+  ctx.closePath();
+  ctx.fillStyle = shadeColor('#2ff99', -20);
+  ctx.strokeStyle = shadeColor('#2f3f99', -60);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(x- 200, y );
+  ctx.lineTo(x - wx , y - h - wx * 0.5 +200);
+  ctx.lineTo(x - wx, y - wx * 0.5);
+  ctx.lineTo(x -200, y - h * 1 );
+  ctx.closePath();
+  ctx.fillStyle = shadeColor('#2ff99', -10);
+  ctx.strokeStyle = shadeColor('#2ff99', -50);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(x- 300, y -50);
+  ctx.lineTo(x - wx -200, y - wx * 0.5 -100);
+  ctx.lineTo(x -200, y - h * 1 );
+  ctx.lineTo(x - wx -100, y - h - wx * 0.5 +150);
+  ctx.closePath();
+  ctx.fillStyle = shadeColor('#4dddd9', -10);
+  ctx.strokeStyle = shadeColor('#4dddd9', -50);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(x- 200, y +100);
+  ctx.lineTo(x - wx , y - h - wx * 0.5 +300);
+  ctx.lineTo(x - wx, y - wx * 0.5 + 100);
+  ctx.lineTo(x -200, y - h * 1 +100);
+  ctx.closePath();
+  ctx.fillStyle = shadeColor('#f99999', -10);
+  ctx.strokeStyle = shadeColor('#f99999', -50);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(x -300, y +50);
+  ctx.lineTo(x - wx - 200, y - h - wx * 0.5 +100);
+  ctx.lineTo(x -200, y - h * 1 +100);
+  ctx.lineTo(x - wx -100, y - wx * 0.5 + 150);
+  ctx.closePath();
+  ctx.fillStyle = shadeColor('#f99999', -10);
+  ctx.strokeStyle = shadeColor('#f99999', -50);
+  ctx.stroke();
+  ctx.fill();
+
+
+  const B = () => {
+
+  }
+
 
 }
